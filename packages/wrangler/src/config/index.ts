5bc416c131992dd5494bdec5b9ded819dd979b98
Samuel Macleod <smacleod@cloudflare.com>
2024-10-15 11:41:36 +0100

    Cleanup

--- packages/wrangler/src/config/index.ts
+++ packages/wrangler/src/config/index.ts
@@ -192,13 +192,19 @@ export function findWranglerToml(
 	return findUpSync(`wrangler.toml`, { cwd: referencePath });
 }
 
+function addLocalSuffix(id: string, local: boolean = false) {
+	return `${id}${local ? " (local)" : ""}`;
+}
+
 /**
  * Print all the bindings a worker using a given config would have access to
  */
 export function printBindings(
 	bindings: CfWorkerInit["bindings"],
-	registry?: WorkerRegistry,
-	local?: boolean
+	context: {
+		registry?: WorkerRegistry;
+		local?: boolean;
+	} = {}
 ) {
 	const truncate = (item: string | Record<string, unknown>) => {
 		const s = typeof item === "string" ? item : JSON.stringify(item);
@@ -257,7 +263,7 @@ export function printBindings(
 				({ name, class_name, script_name }) => {
 					let value = class_name;
 					if (script_name) {
-						const registryDefinition = registry?.[script_name];
+						const registryDefinition = context.registry?.[script_name];
 						if (
 							registryDefinition &&
 							registryDefinition.durableObjects.some(
@@ -285,7 +291,7 @@ export function printBindings(
 			entries: kv_namespaces.map(({ binding, id }) => {
 				return {
 					key: binding,
-					value: `${id}${local ? " (local)" : ""}`,
+					value: addLocalSuffix(id, context.local),
 				};
 			}),
 		});
@@ -314,7 +320,7 @@ export function printBindings(
 			entries: queues.map(({ binding, queue_name }) => {
 				return {
 					key: binding,
-					value: `${queue_name}${local ? " (local)" : ""}`,
+					value: addLocalSuffix(queue_name, context.local),
 				};
 			}),
 		});
@@ -335,7 +341,7 @@ export function printBindings(
 					}
 					return {
 						key: binding,
-						value: `${databaseValue}${local ? " (local)" : ""}`,
+						value: addLocalSuffix(databaseValue, context.local),
 					};
 				}
 			),
@@ -348,7 +354,7 @@ export function printBindings(
 			entries: vectorize.map(({ binding, index_name }) => {
 				return {
 					key: binding,
-					value: `${index_name}${local ? " (local)" : ""}`,
+					value: addLocalSuffix(index_name, context.local),
 				};
 			}),
 		});
@@ -360,7 +366,7 @@ export function printBindings(
 			entries: hyperdrive.map(({ binding, id }) => {
 				return {
 					key: binding,
-					value: `${id}${local ? " (local)" : ""}`,
+					value: addLocalSuffix(id, context.local),
 				};
 			}),
 		});
@@ -375,7 +381,7 @@ export function printBindings(
 				}
 				return {
 					key: binding,
-					value: `${bucket_name}${local ? " (local)" : ""}`,
+					value: addLocalSuffix(bucket_name, context.local),
 				};
 			}),
 		});
@@ -402,7 +408,7 @@ export function printBindings(
 					value += `#${entrypoint}`;
 				}
 
-				const registryDefinition = registry?.[service];
+				const registryDefinition = context.registry?.[service];
 				if (
 					registryDefinition &&
 					(!entrypoint || registryDefinition.entrypointAddresses?.[entrypoint])
